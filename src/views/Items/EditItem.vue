5c6473f0d9742d9b77be8b8cc9575337ff158a9f
Sarang <sarang98divakaran@gmail.com>
2020-03-26 16:23:03 +0530

    fixed issue where category id was not sent to server

--- src/views/Items/EditItem.vue
+++ src/views/Items/EditItem.vue
@@ -142,6 +142,8 @@ export default {
         },
         upload() {
             const data = this.item;
+            // assign category id from selected category
+            data.category_id = this.category.category_id;
 
             // remove keys with null or undefined or 0 value.
             for (let key in data) {
