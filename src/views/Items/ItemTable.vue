9dd293389b7aed5b33cbae79713408dc85de7a2c
Sarang <sarang98divakaran@gmail.com>
2020-03-27 17:29:39 +0530

    removed type prop

--- src/views/Items/ItemTable.vue
+++ src/views/Items/ItemTable.vue
@@ -1,11 +1,9 @@
 <template>
     <div>
-        <div class="card-header border-0"
-            :class="type === 'dark' ? 'bg-transparent': ''"
-        >
+        <div class="card-header border-0">
             <div class="row align-items-center">
                 <div class="col">
-                    <h3 class="mb-0" :class="type === 'dark' ? 'text-white': ''">
+                    <h3 class="mb-0">
                         Add Items
                     </h3>
                     <div class="col-6">
@@ -28,8 +26,6 @@
         </div>
         <div class="table-responsive">
             <base-table class="table align-items-center table-flush"
-                :class="type === 'dark' ? 'table-dark': ''"
-                :thead-classes="type === 'dark' ? 'thead-dark': 'thead-light'"
                 tbody-classes="list"
                 :data="excel"
             >
@@ -114,11 +110,6 @@ import XLSX from 'xlsx';
 
 export default {
     name: 'item-table',
-    props: {
-        type: {
-            type: String
-        },
-    },
     data: () => ({
         excel: [],
         category: {
