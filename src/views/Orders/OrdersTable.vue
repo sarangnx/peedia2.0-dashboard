f2ccfed8a8e719bfe6d7465d1a6991a7b5f5d8ef
Sarang <sarang98divakaran@gmail.com>
2020-03-29 00:01:35 +0530

    added loading component to order

--- src/views/Orders/OrdersTable.vue
+++ src/views/Orders/OrdersTable.vue
@@ -158,14 +158,14 @@
         </div>
         <!-- Delete Modal -->
         <modal :show.sync="deleteModal" gradient="danger">
-            <template slot="header">
+            <template slot="header" v-if="!deleteLoading">
                 <h5 class="modal-title">Delete Order</h5>
             </template>
             <div class="py-1 text-center" v-show="!deleteLoading">
                 <h4 class="heading mt-4">Are you sure you want to delete this order?</h4>
                 <p class="text-white">This action cannot be reverted.</p>
             </div>
-            <div class="loader" v-if="deleteLoading">Loading...</div>
+            <loading v-if="deleteLoading" color="white"></loading>
             <template slot="footer" v-if="!deleteLoading">
                 <base-button type="white"
                     @click="deleteOrder()"
@@ -467,65 +467,6 @@ export default {
 };
 </script>
 <style>
-.loader,
-.loader:before,
-.loader:after {
-  background: #ffffff;
-  -webkit-animation: load1 1s infinite ease-in-out;
-  animation: load1 1s infinite ease-in-out;
-  width: 1em;
-  height: 4em;
-}
-.loader {
-  color: #ffffff;
-  text-indent: -9999em;
-  margin: 88px auto;
-  position: relative;
-  font-size: 11px;
-  -webkit-transform: translateZ(0);
-  -ms-transform: translateZ(0);
-  transform: translateZ(0);
-  -webkit-animation-delay: -0.16s;
-  animation-delay: -0.16s;
-}
-.loader:before,
-.loader:after {
-  position: absolute;
-  top: 0;
-  content: '';
-}
-.loader:before {
-  left: -1.5em;
-  -webkit-animation-delay: -0.32s;
-  animation-delay: -0.32s;
-}
-.loader:after {
-  left: 1.5em;
-}
-@-webkit-keyframes load1 {
-  0%,
-  80%,
-  100% {
-    box-shadow: 0 0;
-    height: 4em;
-  }
-  40% {
-    box-shadow: 0 -2em;
-    height: 5em;
-  }
-}
-@keyframes load1 {
-  0%,
-  80%,
-  100% {
-    box-shadow: 0 0;
-    height: 4em;
-  }
-  40% {
-    box-shadow: 0 -2em;
-    height: 5em;
-  }
-}
 .input__height > input {
     height: 2.2em;
     padding: 0px 10px;
