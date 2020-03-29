e6050d9004edef8ba5bace8acf4c7ed4e1c5e41c
Sarang <sarang98divakaran@gmail.com>
2020-03-29 23:18:07 +0530

    change password form

--- src/views/Auth/Reset.vue
+++ src/views/Auth/Reset.vue
@@ -2,19 +2,40 @@
     <div class="row justify-content-center">
         <div class="col-lg-5 col-md-7">
             <div class="card bg-secondary shadow border-0">
-                <div class="card-body px-lg-5 py-lg-5">
-                    <div class="text-center text-muted mb-4">Reset Password</div>
-                    <form role="form" method="post" action="#" @submit.prevent="login">
+                <div class="card-body px-lg-5 py-lg-5" v-if="!gotCode">
+                    <div class="text-center text-muted mb-4 font-weight-bold">Reset Password</div>
+                    <div>
                         <base-input
                             class="input-group-alternative mb-3"
                             placeholder="Email"
-                            addon-left-icon="ni ni-email-83"
+                            addon-left-icon="fas fa-at"
                             v-model="username"
                         ></base-input>
                         <div class="text-center">
-                            <base-button type="primary" class="my-4" @click.prevent="sendOtp">Send OTP</base-button>
+                            <base-button block type="primary" class="my-4" @click.prevent="sendOtp">Send OTP</base-button>
                         </div>
-                    </form>
+                    </div>
+                    <div class="text-center text-muted">
+                        <base-button type="link" @click="gotCode = true">Got a code?</base-button>
+                    </div>
+                </div>
+                <div class="card-body px-lg-5 py-lg-5" v-else>
+                    <div class="text-center text-muted mb-4 font-weight-bold">Reset Password</div>
+                    <div>
+                        <base-input
+                            class="input-group-alternative mb-3"
+                            placeholder="Password"
+                            type="password"
+                            addon-left-icon="fas fa-key"
+                            v-model="password"
+                        ></base-input>
+                        <div class="text-center">
+                            <base-button block type="primary" class="my-4" @click.prevent="changePassword">Change Password</base-button>
+                        </div>
+                    </div>
+                    <div class="text-center text-muted">
+                        <base-button type="link" @click="gotCode = false">Resend Code</base-button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -25,6 +46,8 @@ export default {
     name: "reset",
     data: () => ({
         username: null,
+        gotCode: false,
+        password: null,
     }),
     methods: {
         sendOtp() {
@@ -40,12 +63,16 @@ export default {
             }).then((response) => {
                 if (response.data && response.data.status === "success") {
                     this.$success('OTP Sent to email');
+                    this.gotCode = true;
                 } else {
                     throw new Error('Unable to send email.');
                 }
             }).catch(() => {
                 this.$error('Unable to send email.');
             });
+        },
+        changePassword() {
+
         }
     }
 };
