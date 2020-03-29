ed8ba6a529f9d05873266d89892019e4d0ff5a3d
Sarang <sarang98divakaran@gmail.com>
2020-03-30 00:17:36 +0530

    transitions

--- src/views/Auth/Reset.vue
+++ src/views/Auth/Reset.vue
@@ -1,60 +1,73 @@
 <template>
     <div class="row justify-content-center">
         <div class="col-lg-5 col-md-7">
-            <div class="card bg-secondary shadow border-0">
-                <div class="card-body px-lg-5 py-lg-5" v-if="!gotCode">
-                    <div class="text-center text-muted mb-4 font-weight-bold">Reset Password</div>
-                    <div>
-                        <base-input
-                            class="input-group-alternative mb-3"
-                            placeholder="Email"
-                            addon-left-icon="fas fa-at"
-                            v-model="username"
-                        ></base-input>
-                        <div class="text-center">
-                            <base-button block type="primary" class="my-4" @click.prevent="sendOtp">Send OTP</base-button>
+            <div class="card bg-secondary shadow border-0" :key="1" v-if="!gotCode">
+                <fade-transition :duration="200" origin="center center">
+                    <div class="card-body px-lg-5 py-lg-5" :key="1">
+                        <div class="text-center text-muted mb-4 font-weight-bold">Reset Password</div>
+                        <div>
+                            <base-input
+                                class="input-group-alternative mb-3"
+                                placeholder="Email"
+                                addon-left-icon="fas fa-at"
+                                v-model="username"
+                            ></base-input>
+                            <div class="text-center">
+                                <base-button block type="primary" class="my-4" @click.prevent="sendOtp">Send OTP</base-button>
+                            </div>
+                        </div>
+                        <div class="text-center text-muted">
+                            <base-button type="link" @click="gotCode = true">Got a code?</base-button>
                         </div>
                     </div>
-                    <div class="text-center text-muted">
-                        <base-button type="link" @click="gotCode = true">Got a code?</base-button>
-                    </div>
-                </div>
-                <div class="card-body px-lg-5 py-lg-5" v-else>
-                    <div class="text-center text-muted mb-4 font-weight-bold">Reset Password</div>
-                    <div v-if="step === 1">
-                        <base-input
-                            class="input-group-alternative mb-3"
-                            placeholder="OTP"
-                            addon-left-icon="fas fa-lock"
-                            v-model="password"
-                        ></base-input>
-                        <div class="text-center">
-                            <base-button block type="primary" class="my-4" @click.prevent="verifyOtp">Verify</base-button>
+                </fade-transition>
+            </div>
+            <div class="card bg-secondary shadow border-0" :key="2" v-else>
+                <fade-transition :duration="200" origin="center center">
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <div class="text-center text-muted mb-4 font-weight-bold">Reset Password</div>
+                        <fade-transition :duration="200" origin="center center" group>
+                            <div v-if="step === 1" :key="1">
+                                <base-input
+                                    class="input-group-alternative mb-3"
+                                    placeholder="OTP"
+                                    addon-left-icon="fas fa-lock"
+                                    v-model="password"
+                                ></base-input>
+                                <div class="text-center">
+                                    <base-button block type="primary" class="my-4" @click.prevent="verifyOtp">Verify</base-button>
+                                </div>
+                            </div>
+                            <div v-else :key="2">
+                                <base-input
+                                    class="input-group-alternative mb-3"
+                                    placeholder="Password"
+                                    type="password"
+                                    addon-left-icon="fas fa-key"
+                                    v-model="password"
+                                ></base-input>
+                                <div class="text-center">
+                                    <base-button block type="primary" class="my-4" @click.prevent="changePassword">Change Password</base-button>
+                                </div>
+                            </div>
+                        </fade-transition>
+                        <div class="text-center text-muted" v-if="step === 1">
+                            <base-button type="link" @click="gotCode = false">Resend Code</base-button>
                         </div>
                     </div>
-                    <div v-else>
-                        <base-input
-                            class="input-group-alternative mb-3"
-                            placeholder="Password"
-                            type="password"
-                            addon-left-icon="fas fa-key"
-                            v-model="password"
-                        ></base-input>
-                        <div class="text-center">
-                            <base-button block type="primary" class="my-4" @click.prevent="changePassword">Change Password</base-button>
-                        </div>
-                    </div>
-                    <div class="text-center text-muted" v-if="step === 1">
-                        <base-button type="link" @click="gotCode = false">Resend Code</base-button>
-                    </div>
-                </div>
+                </fade-transition>
             </div>
         </div>
     </div>
 </template>
 <script>
+import { FadeTransition } from 'vue2-transitions';
+
 export default {
-    name: "reset",
+    name: 'reset',
+    components: {
+        FadeTransition,
+    },
     data: () => ({
         username: null,
         gotCode: false,
@@ -84,10 +97,11 @@ export default {
             });
         },
         verifyOtp() {
-
+                this.step=2;
+                
         },
         changePassword() {
-
+            this.step=1;
         }
     }
 };
